8c3c3f3a8cfbc8c4ba393065a51fbc9cda5af80a
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2024-08-26 10:18:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 12834603
+    API_HASH = "84a5daf7ac334a70b3fbd180616a76c6"
+    TOKEN = "6641509069:AAH5wv7yTY1zrOxd_fjnO4IR-JCbpSA7xlQ"  
+    START_PIC = "https://graph.org/file/6b86954369716aad14d74.jpg" 
+    CHAT = "https://t.me/ELENAMUSICSUPPORT"    
